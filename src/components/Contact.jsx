6164cfc7f497595c48e14bdfb624e8b1d8ca6986
BlakeLawrence <blake-lawrence@hotmail.co.uk>
2022-09-11 17:09:47 +0100

    changed footer

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,7 +4,7 @@ const Contact = () => {
   return (
     <div
       name="contact"
-      className="pb-24 pl-16 sm:pl-0 w-full h-full bg-[#0a192f] flex justify-center items-center p-4"
+      className="pb-16 pl-16 sm:pl-0 w-full h-full bg-[#0a192f] flex justify-center items-center p-4"
     >
       <form
         method="POST"
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,9 +4,9 @@ import { Link } from "react-scroll";
 
 const Footer = () => {
   return (
-    <footer className="w-full h-[80px] sm:h-[80px] flex justify-around items-center bg-[#0a192f] text-[#ccd6f6] shadow-white shadow-lg">
+    <footer className=" w-full h-[80px] sm:h-[80px] flex justify-around items-center bg-[#0a192f] text-[#ccd6f6] ] ">
       <ul>
-        <li className="mt-3">Copyright &copy;2022</li>
+        <li className=" mt-3">Copyright &copy;2022</li>
       </ul>
     </footer>
   );
