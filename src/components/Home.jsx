5acdb20a00189414e2515041373c17ada0c7b07f
BlakeLawrence <blake-lawrence@hotmail.co.uk>
2022-09-11 13:49:46 +0100

    worked on social icons shadows

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -12,18 +12,18 @@ const Home = () => {
       {/* social icons */}
       <div className="flex fixed flex-col top-[35%] left-1">
         <ul>
-          <li className=" w-[150px] h-[40px] sm:w-[150px] sm:h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 border-2 rounded-md border-[#1982C4] hover:shadow-md hover:shadow-[#1982C4]">
+          <li className=" w-[150px] h-[40px] sm:w-[150px] sm:h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 border-2 rounded-md border-[#1982C4] hover:shadow-inner hover:shadow-[#1982C4]">
             <a
-              className="flex justify-between items-center w-full text-gray-300"
+              className="flex justify-between items-center w-full text-gray-300 font-semibold"
               href="https://www.linkedin.com/in/blake-lawrence83/"
               target="_blank"
             >
               LinkedIn <FaLinkedin size={30} />
             </a>
           </li>
-          <li className=" w-[150px] h-[40px] sm:w-[150px] mt-2 sm:h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 border-2 rounded-md border-[#1982C4] hover:shadow-md hover:shadow-[#1982C4]">
+          <li className=" w-[150px] h-[40px] sm:w-[150px] mt-2 sm:h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 border-2 rounded-md border-[#1982C4] hover:shadow-inner hover:shadow-[#1982C4]">
             <a
-              className="flex justify-between items-center w-full text-gray-300"
+              className="flex justify-between items-center w-full text-gray-300 font-semibold"
               href="https://github.com/BlakeLawrence"
               target="_blank"
             >
@@ -31,18 +31,18 @@ const Home = () => {
             </a>
           </li>
           <Link to="contact" spy={true} smooth={true} duration={500}>
-            <li className=" w-[150px] h-[40px] sm:w-[150px] mt-2 sm:h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 rounded-md border-2 border-[#1982C4] hover:shadow-md hover:shadow-[#1982C4]">
+            <li className=" w-[150px] h-[40px] sm:w-[150px] mt-2 sm:h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 rounded-md border-2 border-[#1982C4] hover:shadow-inner hover:shadow-[#1982C4]">
               <a
-                className="flex justify-between items-center w-full text-gray-300"
+                className="flex justify-between items-center w-full text-gray-300 font-semibold"
                 href="/"
               >
                 Email <HiOutlineMail size={30} />
               </a>
             </li>
           </Link>
-          <li className=" w-[150px] h-[40px] sm:w-[150px] mt-2 sm:h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 border-2 rounded-md border-[#1982C4] hover:shadow-md hover:shadow-[#1982C4]">
+          <li className=" w-[150px] h-[40px] sm:w-[150px] mt-2 sm:h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 border-2 rounded-md border-[#1982C4] hover:shadow-inner hover:shadow-[#1982C4]">
             <a
-              className="flex justify-between items-center w-full text-gray-300"
+              className="flex justify-between items-center w-full text-gray-300 font-semibold"
               href={cv}
               target="_blank"
             >
