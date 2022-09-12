f7fd6885e46b059932925e6acf6977a0532caaef
BlakeLawrence <blake-lawrence@hotmail.co.uk>
2022-09-12 11:19:50 +0100

    adjusted font colors

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -55,14 +55,17 @@ const Home = () => {
       <div name="home" className="w-full h-screen bg-[#1c3144]">
         {/* Overall Container */}
         <div className="items-start max-w-[1000px] mx-10 lg:mx-auto px-8 flex flex-col justify-center h-full pt-8">
-          <p className="text-[#1982C4] text-xl sm:text-3xl"> Hi, my name is</p>
+          <p className="text-[#1982C4] text-xl sm:text-3xl font-medium">
+            {" "}
+            Hi, my name is
+          </p>
           <h1 className="ml-0 sm:ml-[-4px] text-3xl sm:text-8xl font-bold text-[#ccd6f6]">
             Blake Lawrence
           </h1>
           <h2 className="text-3xl sm:text-6xl font-bold text-[#8892b0]">
             I'm a Full Stack Developer.
           </h2>
-          <p className=" text-[#8892b0] py-4 max-w-[700px]">
+          <p className="text-gray-300 py-4 max-w-[700px]">
             I am a highly motivated, ambitious and detail orientated Individual.
             I love learning new things every day and I am always pushing myself
             to be the best I can.
