72980482a1febd5faf182fcb6d600580af063a69
BlakeLawrence <blake-lawrence@hotmail.co.uk>
2022-09-21 09:58:53 +0100

    changed project card sizes and added more description

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -28,18 +28,19 @@ const Projects = () => {
           {/* Project Cleanup  */}
           <div
             style={{ backgroundImage: `url(${projectCleanup})` }}
-            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div bg-cover bg-center h-[250px]"
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div bg-cover bg-center h-[290px]"
           >
             {/* description */}
           </div>
-          <div className="text-gray-300 text-sm sm:text-md flex flex-col  content-div sm:text-md border-[1px] rounded-md p-2.5  border-[#213b52] bg-[#213b52] shadow-lg shadow-[#040c16] bg-cover bg-center h-[250px]">
+          <div className="text-gray-300 text-sm sm:text-md flex flex-col  content-div sm:text-md border-[1px] rounded-md p-2.5  border-[#213b52] bg-[#213b52] shadow-lg shadow-[#040c16] bg-cover bg-center h-[290px]">
             <h3 className="text-gray-200 text-xl sm:text-2xl font-bold">
               Project Cleanup
             </h3>
 
             <p className="text-gray-300 font-bold">
-              School of Code Final Project
+              School of Code Final Project ( duration - 4 weeks )
             </p>
+            <br />
             <p className="text-gray-300 text-left">
               My team and I chose to build an app to help tackle the world's
               pollution crisis.
@@ -51,7 +52,7 @@ const Projects = () => {
             </p>
 
             {/* Buttons div */}
-            <div className="pt-3 sm:pt-6">
+            <div className="flex justify-center">
               <a href="https://projectcleanup.netlify.app/" target="_blank">
                 <button className="text-center rounded-lg px-2 py-1.5 ml-0 mr-2 hover:bg-[#1982c4] text-gray-300 hover:text-white border-[1px] hover:border-[#1982c4] sm:text-md">
                   Demo
@@ -75,11 +76,11 @@ const Projects = () => {
           {/* Squatify  */}
           <div
             style={{ backgroundImage: `url(${squatify})` }}
-            className="text-gray-300  shadow-lg shadow-[#040c16] container rounded-md flex justify-center items-center mx-auto content-div bg-cover bg-center h-[250px]"
+            className="text-gray-300  shadow-lg shadow-[#040c16] container rounded-md flex justify-center items-center mx-auto content-div bg-cover bg-center h-[290px]"
           >
             {/* description */}
           </div>
-          <div className=" text-gray-300 text-sm sm:text-md flex flex-col container content-div sm:text-md border-[1px] rounded-lg p-2.5 border-[#213b52] bg-[#213b52] shadow-lg shadow-[#040c16] bg-cover bg-center h-[250px]">
+          <div className=" text-gray-300 text-sm sm:text-md flex flex-col container content-div sm:text-md border-[1px] rounded-lg p-2.5 border-[#213b52] bg-[#213b52] shadow-lg shadow-[#040c16] bg-cover bg-center h-[290px]">
             <h3 className="text-2xl font-bold text-gray-200">Squatify</h3>
             <br />
 
@@ -87,8 +88,10 @@ const Projects = () => {
               A side project I initiaded with a few fellow students from School
               of code.
             </p>
-            <p>Helped us keep busy on weekends</p>
-            <p>Practiced Agile team working</p>
+            <p>
+              This helped us keep busy on weekends and provided more teamwork
+              and collaboration experience in an Agile way.
+            </p>
 
             {/* Buttons div */}
             <div className="pt-6">
@@ -111,17 +114,19 @@ const Projects = () => {
           {/* Movie Mate */}
           <div
             style={{ backgroundImage: `url(${movie})` }}
-            className="text-gray-300 shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div bg-cover bg-center h-[250px]"
+            className="text-gray-300 shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div bg-cover bg-center h-[290px]"
           >
             {/* description*/}
           </div>
-          <div className="text-gray-300 text-sm sm:text-md flex flex-col container content-div sm:text-md border-[1px] rounded-md p-2.5 justify-evenly border-[#213b52] bg-[#213b52] shadow-lg shadow-[#040c16] bg-cover bg-center h-[250px]">
+          <div className="text-gray-300 text-sm sm:text-md flex flex-col container content-div sm:text-md border-[1px] rounded-md p-2.5 justify-evenly border-[#213b52] bg-[#213b52] shadow-lg shadow-[#040c16] bg-cover bg-center h-[290px]">
             <h3 className="text-2xl font-bold text-gray-200 ">Movie Mate</h3>
             <br />
-            <p>An app that finds all movies by title</p>
+            <p>An app that finds all movies by search title</p>
             <p>
-              Had alot of fun buiding this and cemented some core fundamentals
-              such as fetching data and conditional rendering
+              I had alot of fun buiding this app and it provided an opportunity
+              to cement some of the core React fundamentals such as fetching
+              data, using props and state, conditional rendering and side
+              effects.
             </p>
 
             {/* Buttons div */}
@@ -145,15 +150,21 @@ const Projects = () => {
           {/* To Do list */}
           <div
             style={{ backgroundImage: `url(${todo})` }}
-            className="text-gray-300 shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div bg-cover bg-center h-[250px]"
+            className="text-gray-300 shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div bg-cover bg-center h-[290px]"
           >
             {/* description*/}
           </div>
-          <div className="text-gray-300 text-sm sm:text-md flex flex-col container content-div sm:text-md border-[1px] rounded-md p-2.5 justify-evenly border-[#213b52] bg-[#213b52] shadow-lg shadow-[#040c16] bg-cover bg-center h-[250px]">
+          <div className="text-gray-300 text-sm sm:text-md flex flex-col container content-div sm:text-md border-[1px] rounded-md p-2.5 justify-evenly border-[#213b52] bg-[#213b52] shadow-lg shadow-[#040c16] bg-cover bg-center h-[290px]">
             <h3 className="text-2xl font-bold text-gray-200 ">To Do List</h3>
             <br />
-            <p>a basic react app with no back-end.</p>
-            <p>I built this to practice React Basics</p>
+            <p>
+              This is a basic react app I built while learning React to practice
+              the basics we had been taught.
+            </p>
+            <p>
+              You can add or delete todo's and mark them as complete when
+              they've been done.
+            </p>
 
             {/* Buttons div */}
             <div className="pt-8">
@@ -175,17 +186,28 @@ const Projects = () => {
           {/* Sourcery */}
           <div
             style={{ backgroundImage: `url(${sourcery})` }}
-            className="text-gray-300 shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div bg-cover bg-center h-[250px]"
+            className="text-gray-300 shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div bg-cover bg-center h-[325px]"
           >
             {/* description*/}
           </div>
-          <div className="text-gray-300 text-sm sm:text-md flex flex-col container content-div sm:text-md border-[1px] rounded-md p-2.5 justify-evenly border-[#213b52] bg-[#213b52] shadow-lg shadow-[#040c16] bg-cover bg-center h-[250px]">
+          <div className="text-gray-300 text-sm sm:text-md flex flex-col container content-div sm:text-md border-[1px] rounded-md p-2.5 justify-evenly border-[#213b52] bg-[#213b52] shadow-lg shadow-[#040c16] bg-cover bg-center h-[325px]">
             <h3 className="text-2xl font-bold text-gray-200 ">
               Sourcery Resources
             </h3>
+            <p className="text-gray-300 font-bold">
+              Week-9 Project ( duration- 1 week )
+            </p>
             <br />
-            <p>Project week 9 project</p>
-            <p>An app to help students find learning resources</p>
+            <p>An app to help students find learning resources.</p>
+            <p>
+              This project was our first taste of collaborating in a random
+              Agile team under pressure to engineer a full stack application.
+            </p>
+            <p>
+              It was a pressure filled week but an enjoyable one that helped us
+              build resillience and provided invaluable experience for our final
+              project month.
+            </p>
 
             {/* Buttons div */}
             <div className="pt-8">
