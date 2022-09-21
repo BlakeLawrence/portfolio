843ff5a5802ec5d4cecbfa69d216170e9c95480e
BlakeLawrence <blake-lawrence@hotmail.co.uk>
2022-09-21 11:23:32 +0100

    added a back to  top option in footer to scroll user back to the home section

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,14 @@
 import React from "react";
-import Logo from "../assets/Blake-Logo.svg";
 import { Link } from "react-scroll";
 
 const Footer = () => {
   return (
     <footer className=" w-full h-[80px] sm:h-[80px] flex justify-around items-center bg-[#1c3144] text-[#ccd6f6] ] ">
-      <ul>
-        <li className=" mt-3">Copyright &copy;2022</li>
+      <ul className="flex">
+        <li className=" mt-3">Copyright &copy; 2022</li>
+        <Link to="home" spy={true} smooth={true} duration={500}>
+          <li className=" mt-3">back to top ⬆️</li>
+        </Link>
       </ul>
     </footer>
   );
