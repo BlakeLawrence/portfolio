0cf83404ec742f6a02428eec3ac1befa67604a4e
BlakeLawrence <blake-lawrence@hotmail.co.uk>
2023-02-16 16:35:51 +0000

    also chnaged CV for mobile view

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { FaGithub, FaLinkedin } from "react-icons/fa";
 import { BsFillPersonLinesFill } from "react-icons/bs";
-import cv from "../assets/cv2022.pdf";
+import cv from "../assets/cv2023.pdf";
 
 const Contact = () => {
   return (
